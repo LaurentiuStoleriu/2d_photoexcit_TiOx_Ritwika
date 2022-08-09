d440a8dde0dd70a36940708dc253e9249069b7ba
Laurențiu STOLERIU <65720425+LaurentiuStoleriu@users.noreply.github.com>
2022-08-09 19:20:20 +0300

    2D OK

--- generate_version2d.cpp
+++ generate_version2d.cpp
@@ -1,20 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+const int nPart = 1000000;
+const int nSide = 1000;
+
+double radius = 0.2;
+double distance = 0.6;
+
+struct sReadData
+{
+    double x, y, z, r;
+};
+
+struct sReadData Medium[nPart];
 
 int main()
 {
-    const int nPart = 40000;
-    const int nSide = 200;
-
-    double radius = 0.2;
-    double distance = 0.6;
-
-    struct sReadData
-    {
-        double x, y, z, r;
-    };
-
-    struct sReadData Medium[nPart];
-
     double stepX = distance + 2.0 * radius;
     double stepY = distance + 2.0 * radius;
     double x0 = radius;
@@ -32,7 +33,8 @@ int main()
     }
 
     FILE *fp;
-    fp = fopen("/home/ritwika/data/1.hc4250_Oct'20/results/systems/200x200_r02_d06.dat", "w");
+    //fp = fopen("/home/ritwika/data/1.hc4250_Oct'20/results/systems/200x200_r02_d06.dat", "w");
+    fp = fopen("E:\\Stoleriu\\C\\special\\3d\\generare\\2022\\TiOX\\1000x1000_r02_d06.dat", "w");
     for (int i = 0; i < nPart; i++)
     {
         fprintf(fp, "%20.15lf  %20.15lf  %20.15lf  %20.15lf\n", Medium[i].x, Medium[i].y, Medium[i].z, Medium[i].r);
